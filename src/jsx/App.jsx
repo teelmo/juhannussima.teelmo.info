526b9674564536578535d79d297009b86c38bffa
Teemo Tebest <teemo.tebest@gmail.com>
2022-10-09 22:13:27 +0200

    Updated webpack

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -1,42 +1,20 @@
-import React, {Component} from 'react';
-import style from './../styles/styles.less';
+import React from 'react';
+import '../styles/styles.less';
 
 import Confetti from 'react-confetti';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-    }
-  }
-  componentDidMount() {
-
-  }
-  componentDidUpdate(prevProps, prevState, snapshot) {
-
-  }
-  componentWillUnMount() {
-
-  }
-  // shouldComponentUpdate(nextProps, nextState) {}
-  // static getDerivedStateFromProps(props, state) {}
-  // getSnapshotBeforeUpdate(prevProps, prevState) {}
-  // static getDerivedStateFromError(error) {}
-  // componentDidCatch() {}
-  render() {
-    return (
-      <div className={style.app}>
-        <Confetti width={window.innerWidth} height={window.innerHeight} recycle={true} className={style.rain} />
-        <div className={style.image_container}><img src="img/kokko.jpg" /></div>
-        <h1>You! Yes you!</h1>
-        <div className={style.image_container}><img src="img/juhannus.jpg" /></div>
-        <h3>You are summoned to get drunk on Saturday</h3>
-        <h1>The Finnish way</h1>
-        <div className={style.image_container}><img src="img/humalainen.jpeg" /></div>
-        <h3>Be there or don't come to Finland, ever!!</h3>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="app">
+      <Confetti width={window.innerWidth} height={window.innerHeight} recycle className="rain" />
+      <div className="image_container"><img src="./assets/img/kokko.jpg" alt="Meme" /></div>
+      <h1>You! Yes you!</h1>
+      <div className="image_container"><img src="./assets/img/juhannus.jpg" alt="Meme" /></div>
+      <h3>You are summoned to get drunk on Saturday</h3>
+      <h1>The Finnish way</h1>
+      <div className="image_container"><img src="./assets/img/humalainen.jpeg" alt="Meme" /></div>
+      <h3>Be there or don&apos;t come to Finland, ever!!</h3>
+    </div>
+  );
 }
 export default App;
